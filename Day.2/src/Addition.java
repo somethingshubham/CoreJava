5d03dae18261018803f02f065143c429e54213d5
Student <Student@DESKTOP-BFITOTC>
2022-03-02 05:30:30 -0800

    Day2

---
+++ Day.2/src/Addition.java
@@ -0,0 +1,11 @@
+
+public class Addition {
+	 public static void main(String[] args) {
+		  int a=200;
+		  int b=20;
+		  int c= a+b;
+		  
+		  System.out.println("The Sum of a  and b is "+c);
+	 }
+
+}

bf4fdb94818d80d21092aad1a0003aeebc1e1eac
Student <Student@DESKTOP-BFITOTC>
2022-03-20 13:50:27 -0700

    Day11

---
+++ Day11/src/Junior_dev/CarModel.java
@@ -0,0 +1,13 @@
+package Junior_dev;
+
+public class CarModel
+{
+	public void display()
+	{
+		System.out.println("Tata");
+		System.out.println("Maruti");
+		
+		
+	}
+
+}
---
+++ Day11/src/Senior_dev/Tata_Car.java
@@ -0,0 +1,11 @@
+package Senior_dev;
+import Junior_dev.CarModel;
+public class Tata_Car {
+
+	public static void main (String[]args)
+	{
+		CarModel c=new CarModel();
+		c.display();
+	}
+
+}
